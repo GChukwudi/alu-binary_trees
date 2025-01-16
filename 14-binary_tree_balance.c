25f02e6b0ae914288fbb43efc63cba3df6c00c66
GChukwudi <g.chukwudi@alustudent.com>
2025-01-16 19:20:31 +0200

    count tree nodes

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -11,10 +11,10 @@ int binary_tree_balance(const binary_tree_t *tree)
 	int left_height, right_height;
 
 	if (tree == NULL)
-		return (0);
+		return (-1);
 
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
-	return (left_height - right_height);
+	return ((left_height > right_height) ? left_height : right_height + 1);
 }
