2e474bb37f51d42f04ea49640d40ceed111724ae
GChukwudi <g.chukwudi@alustudent.com>
2025-01-16 19:32:28 +0200

    updated

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,13 +8,11 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left_height, right_height;
-
 	if (tree == NULL)
-		return (-1);
+		return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	int left_height = binary_tree_height(tree->left);
+	int right_height = binary_tree_height(tree->right);
 
-	return ((left_height > right_height) ? left_height : right_height + 1);
+	return (left_height - right_height);
 }
